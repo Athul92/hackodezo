a4938b7ea31c7989cca626008e6702f7b5ac2303
Athul(hackodezo) <athul.s@relyonsoft.com>
2017-03-15 20:43:41 +0530

    server change

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server '128.199.118.148', user: 'deploy', roles: %w(web app db)
+server '139.59.61.226', user: 'deploy', roles: %w(web app db)
 
 # Don't change these unless you know what you're doing
 set :pty,             true
